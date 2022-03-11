154f47ab9240d92a0bbec6d2cbdb1d0300ae8e23
Donald Farrar <donfar09@gmail.com>
2022-03-11 13:50:56 -0800

    working flow in the recently ASKED questons widget

--- src/widgets/Questions.jsx
+++ src/widgets/Questions.jsx
@@ -4,18 +4,40 @@ function Questions() {
       <div className="questions-container">
         <h1>Stuff is going to go here!</h1>
         <ul>
-          <li>Here is some stuff</li>
-          <li>More stuff</li>
-          <li>LINKS WILL BE HERE</li>
           <ol>
-            <li>here is an un ordered list of more things</li>
-            <li>more things</li>
-            <li>more</li>
-            <li>LINKS WILL BE HERE</li>
-            <li>...more</li>
+            <h4>Trying to create an user account.</h4>
+            <p>Follow these steps: </p>
+            <li>Go to sign in page.</li>
+            <li>Click registeration Link</li>
+            <li>Fill out all fields</li>
+            <li>Click the Register Me Button</li>
+            {/* click here: //link */}
           </ol>
+          <ol>
+            <h4>Problem Signing in.</h4>
+            <p>Follow these steps: </p>
+            <li>attempt the following:</li>
+            <li>input your username and password then click sign in</li>
+            <li>
+              if you forgot your password or username, email staff and request a
+              link to reset them
+            </li>
+            {/* click here: //link */}
+          </ol>
+          <ol>
+            <h4> Problem creating a bank account</h4>
+            <p>follow these steps</p>
+            <li>
+              if you don't have an account then create an account using the
+              steps above in "creating an account list"
+            </li>
+            <li> navigate to the create a bank account page</li>
+            {/*click here //link */}
+            <li>login in using your username and password</li>
+            <li>create an account: checking or saving</li>
+          </ol>
+          <li></li>
         </ul>
-        <h2>some cool link maybe to somewhere here I dont know man</h2>
       </div>
     </>
   );
